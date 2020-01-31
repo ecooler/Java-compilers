77a7e268217d8c651f072a859b33039d61cd9237
PowCoder <31611767+ecooler@users.noreply.github.com>
2020-01-31 15:19:19 +0800

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,12 @@
+// Do not modify the code below!  Don't add anything (including
+// "public" declarations), don't remove anything. Don't wrap it in a
+// package, don't make it an innner class of some other class.
+// If your IDE suggsts to change anything below, ignore your IDE.
+
+class CodegenException extends Exception {
+    public String msg;
+    public CodegenException ( String _msg ) { msg = _msg; } }
+
+interface Codegen {
+    public String codegen ( Program p ) throws CodegenException; }
+
